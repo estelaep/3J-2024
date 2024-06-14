b62f999520606181e74edbd5b9cc481b65ae5b63
estelaep <127420924+estelaep@users.noreply.github.com>
2024-06-14 09:45:07 -0300

    aula 14/06

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
-console.log(tempo);
+tempo[0].textContent="olá";
+const tempoObjetivo1 = new Date(2025, 03, 15, 15, 0);
+const tempoObjetivo2 = new Date(2027, 11, 29, 23, 0);
+const tempoObjetivo3 = new Date(2025, 11, 18, 30, 0);
+const tempoObjetivo4 = new Date(2027, 06, 04, 17, 20);
+const agora = new Date();
+TEMPO[1].textContent = agora
 for(let i = 0; 1 < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){
