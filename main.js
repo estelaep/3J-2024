756c2bb2cf893a642e546a12b50d3f13db8c5742
estelaep <127420924+estelaep@users.noreply.github.com>
2024-07-25 08:07:51 -0300

    aula dia 25/07/2024

--- main.js
+++ main.js
@@ -3,28 +3,39 @@ const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
 tempo[0].textContent="Olá";
 const tempoObjetivo1 = new Date (2025, 03, 15, 15, 00); 
-const tempoObjetivo2 = new Date (2027, 11, 29, 23, 00);
-const tempoObjetivo3 = new Date (2025, 03, 16, 30, 00);
-const tempoObjetivo4 = new Date (2027, 06, 04, 17, 45);
-const agora = new Date() 
+const tempoObjetivo2 = new Date (2025, 11, 29, 23, 00);
+const tempoObjetivo3 = new Date (2024, 03, 16, 30, 00);
+const tempoObjetivo4 = new Date (2025, 06, 04, 17, 45);
+
+function calculaTempo(tempoObjetivo){
+    const agora = new Date()
 let segundos;
 let minutos;
 let horas;
 let dias;
 
-segundos = (tempoObjetivo1-agora)/1000
-minutos = segundos/60
-horas = minutos/60
-dias = horas/24
+segundos=Math.floor((tempoObjetivo-agora)/1000)
+minutos=Math.floor(segundos/60)
+horas=Math.floor(minutos/60)
+dias=Math.floor(horas/24)
 
-segundos=Math.floor(segundos);
-minutos=Math.floor(minutos);
-horas=Math.floor(horas);
-dias=Math.floor(dias);
+segundos = segundos%60
+minutos = minutos%60
+horas = horas%24
+if(segundos >=0){
+    return `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos, ${segundos} segundos`
+} else {
+    return `Prazo finalizado!`
+}
 
 
+}
 
-tempo[0].textContent= `faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`;
+
+tempo[0].textContent = calculaTempo(tempoObjetivo1);
+tempo[1].textContent = calculaTempo(tempoObjetivo2);
+tempo[2].textContent = calculaTempo(tempoObjetivo3);
+tempo[3].textContent = calculaTempo(tempoObjetivo4)
 
 
 
